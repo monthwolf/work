f2907590720a67e3dd0d0fc67efe07f693e77b02
monthwolf <1369755540@qq.com>
2022-11-16 12:30:05 +0000

    modified:   "c\350\257\255\350\250\200\345\255\246\344\271\240/\345\255\227\347\254\246\344\270\262\346\213\274\346\216\245.c" 	modified:   "c\350\257\255\350\250\200\345\255\246\344\271\240/\346\216\222\351\230\237.c"

--- c语言学习/字符串拼接.c
+++ c语言学习/字符串拼接.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-void str_pj(char *a,char *b);
+char *str_pj(char *a,char *b);
 
 int main(void){
-    char str1[80]="Welcome to ",str2[20]="C language";
+    char str1[200],str2[80],*p;
+    printf("please input string1:\n");
+    fgets(str1,200,stdin);
+    fflush(stdin);
+    printf("please input string2:\n");
+    fgets(str2,80,stdin);
     /*
     char *p1,*p2;
-    p1=str1;//指针指向数组首项,*p获取单项的值，p获取整个数列，知道匹配到'\0'停止
+    p1=str1;//指针指向数组首项,*p获取单项的值，p获取整个数列，直到匹配到'\0'停止
     p2=str2;
     while(*p1!='\0')p1++;
     while(*p2!='\0'){
@@ -16,14 +21,15 @@ int main(void){
     p1=str1;//切记将指针返回首项
     printf("%s",p1);
     */
-    str_pj(str1,str2);
+    p=str_pj(str1,str2);
+    printf("%s",p);
 }
 
 //拓展：字符串拼接函数
 
-void str_pj(char *a,char *b){
+char *str_pj(char *a,char *b){
     int i=0;
-    while(*a!='\0'){
+    while(*a!='\0'&&*a!='\n'){
         a++;
         i++;
     }
@@ -31,7 +37,11 @@ void str_pj(char *a,char *b){
         *a=*b;
         a++;
         b++;
+        i++;
     }
-    while(i>0)a--;
-    printf("%s",a);
+    while(i>0){
+        a--;
+        i--;
+    }
+    return a;
 }
